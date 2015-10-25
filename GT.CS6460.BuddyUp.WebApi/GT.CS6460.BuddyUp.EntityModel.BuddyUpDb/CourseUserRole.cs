32c8726d1b2dc708723e1ce871bbee2002c842db
Saurav Jha <sauravgatech@gmail.com>
2015-10-25 02:44:15 -0400

    adding some more changes to db tables

---
+++ GT.CS6460.BuddyUp.WebApi/GT.CS6460.BuddyUp.EntityModel.BuddyUpDb/CourseUserRole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GT.CS6460.BuddyUp.EntityModel.BuddyUpDb
+{
+    public partial class CourseUserRole : AuditBase
+    {
+        [Key, ForeignKey("Course")]
+        public int RoleId { get; set; }
+
+        [Key, ForeignKey("UserProfile")]
+        public int UserId { get; set; }
+
+        [Key, ForeignKey("Role")]
+        public int CourseId { get; set; }
+
+
+    }
+}
---
+++ GT.CS6460.BuddyUp.WebApi/GT.CS6460.BuddyUp.EntityModel.BuddyUpDb/QuestionType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GT.CS6460.BuddyUp.EntityModel.BuddyUpDb
+{
+    public partial class QuestionType
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int QuestionTypeId { get; set; }
+        
+        [StringLength(24)]
+        public string QuestionType { get; set; }
+    }
+}
